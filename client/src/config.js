676addbaa9dc3c6d4fa7acd537a41efb7ea1615a
mhalgan <gccr92@gmail.com>
2020-02-24 13:21:06 -0300

    changed baseUrl on front API

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  apiBaseUrl: "http://192.168.99.100:5000/api"
-  //apiBaseUrl: "/api"
+  //apiBaseUrl: "http://192.168.99.100:5000/api"
+  apiBaseUrl: "/api"
 };
 
 export default config;
